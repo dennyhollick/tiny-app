19f4c00c3cb546fb6911a2ec6958e903a7874389
Denny Hollick <denny@Dennys-MacBook-Pro.local>
2017-07-06 21:12:48 -0700

    Cleaned up code and ES Lint

--- express_server.js
+++ express_server.js
@@ -6,8 +6,8 @@
 //TODO: [-] Redirect /urls to login if not logged in and display error msg
 //TODO: [X] /login If user is logged in, redirect to /urls
 //TODO: [X] /register If user is logged in, redirect to /urls
-//TODO: [ ] Clean up comments
-//TODO: [ ] Remove console.logs
+//TODO: [X] Clean up comments
+//TODO: [X] Remove console.logs
 //TODO: [ ] Refactor and clean where possible, ES LINT
 //TODO: [ ] Final code review
 
@@ -20,6 +20,7 @@
 //STRETCH: [ ] /url & /url:id displays number of unique visits
 //STRETCH: [ ] Make me pretty.
 
+//INITIALIZE
 
 const express = require("express");
 const app = express();
@@ -34,8 +35,10 @@ app.set("view engine", "ejs");
 app.use(cookieSession({
   name: 'session',
   keys: ['iamcornholio'],
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  maxAge: 24 * 60 * 60 * 1000
 }));
+
+//DATABASES
 
 var urlDatabase = {
   "b2xVn2": {longUrl: "http://lighthouselabs.ca", owner: "test" },
@@ -60,200 +63,13 @@ const users = {
   }
 };
 
-
-app.get("/", (req, res) => {
-  res.redirect("/urls");
-});
-
-
-//Serves the login page when requested
-
-app.get("/login", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
-  if (!req.session.user_id){
-    res.render("login", templateVars);
-  } else {
-    res.redirect("/urls");
-  }
-});
-
-//Applies user info to cookie upon login
-
-app.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const userId = doesEmailExist(email);
-  if (userId){
-    if (bcrypt.compareSync(password, users[userId].password)) {
-      console.log(req.session);
-      req.session.user_id = users[userId].id;
-      res.redirect('/urls');
-    } else {
-      res.statusCode = 403;
-      res.send('password is incorrect');
-    }
-  } else {
-    res.statusCode = 403;
-    res.send('the username does not exist');
-  }
-});
-
-//Logout to clear the cookie info
-
-app.post("/logout", (req, res) => {
-  req.session = null;
-  res.redirect("/urls");
-});
-
-//Page to create a new login
-
-app.get("/register", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
-  if (!req.session.user_id){
-    res.render("register", templateVars);
-  } else {
-    res.redirect("/urls");
-  }
-});
-
-//Creates a new user. Compares it to database first to make sure it doesn't exit and user inputs are valid.
-
-app.post("/register", (req, res) => {
-  const userId = randomString();
-  
-  if (req.body.email && req.body.password) {
-    if (doesEmailExist(req.body.email)) {
-      res.statusCode = 400;
-      res.end("Email already exists as a user. Please login with your email and password.");
-    } else {
-      users[userId] = {
-        id: userId,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 10)
-      };
-      console.log(users[userId]);
-      req.session.user_id = users[userId].id;
-      res.redirect("/urls");
-    }
-  } else {
-    res.statusCode = 400;
-    res.end("Error: Enter a valid email and password");
-  }
-});
-
-//Page shows all of the URLs that have been created
-
-app.get("/urls", (req, res) => {
-  let userUrls = returnUrlListForUser(req.session.user_id);
-  let templateVars = { urls: userUrls, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
-  console.log(userUrls);
-  if (req.session.user_id) {
-    res.render("urls_index", templateVars);
-  } else {
-    res.redirect("/login");
-  }
-});
-
-//Page displated to create a new URL
-
-app.get("/urls/new", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
-  if (req.session.user_id) {
-    res.render("urls_new", templateVars);
-  } else {
-    res.redirect("/login");
-  }
-});
-
-//Page displays the short and long URL for a specific URL specified in the path
-
-app.get("/urls/:id", (req, res) => {
-  let urlId = req.params.id;
-  const currentUser = req.session.user_id;
-  if (doesShortURLExist(urlId)) {
-    if (isAuthorizedtoChange(currentUser, urlId)) {
-      let longURL = urlDatabase[urlId].longUrl
-      let templateVars = { shortURL: urlId, longURL: longURL, urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
-      res.render("urls_show", templateVars);
-    } else {
-      res.statusCode = 401;
-      res.send('You are not authorized to view this URL. You do not own it or are not logged in.');
-    }
-  } else {
-    res.statusCode = 404;
-    res.send('Short URL does not exist.');
-  }
-});
-
-//creates a new URL with a new shortlink. Works with /new
-
-app.post("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
-  const currentUser = req.session.user_id;
-  const shortUrl = randomString();
-  const longURL = fixURL(req.body.longURL);
-  urlDatabase[shortUrl] = {longUrl: longURL, owner: currentUser };
-  console.log(shortUrl);
-  console.log(req.body);
-  console.log(urlDatabase);
-  res.redirect("/urls/" + shortUrl);
-});
-
-//To delete a URL
-
-app.post("/urls/:id/delete", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
-  const urlToDelete = req.params.id;
-  const currentUser = req.session.user_id;
-  if ( isAuthorizedtoChange(currentUser, urlToDelete)) {
-    delete urlDatabase[urlToDelete];
-    res.redirect("/urls");
-  } else {
-    res.statusCode = 401;
-    res.send("You are not authorized to delete this URL. Please Login.");
-  }
-});
-
-//To update a URL
-
-app.post("/urls/:id/update", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
-  const shortUrlToUpdate = req.params.id;
-  const updatedLongURL = fixURL(req.body.longURL);
-  const currentUser = req.session.user_id;
-
-  //checks if there is a cookie, and if user is authorized to update URL.
-  if ( isAuthorizedtoChange(currentUser, shortUrlToUpdate)) {
-    urlDatabase[shortUrlToUpdate].longUrl = updatedLongURL;
-    res.redirect("/urls");
-  } else {
-    res.statusCode = 401;
-    res.end("You are not authorized to edit this URL. Please login if you own it.");
-  }
-});
-
-//Generates a random short URL
+//FUNCTIONS
 
 var randomString = function generateRandomString() {
   return (Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1));
 };
 
-//Redirects to short URL if exists. Otherwise 404
-
-app.get("/u/:shortURL", (req, res) => {
-  console.log(urlDatabase[req.params.shortURL]);
-  if (urlDatabase[req.params.shortURL]) {
-    res.redirect(urlDatabase[req.params.shortURL].longUrl);
-  } else {
-    res.statusCode = 404;
-    res.end("Error - Does not exist - Try a different URL or create a new one");
-  }
-});
-
-//function will look through users and check if email already exists in database
-
 function doesEmailExist (emailInput) {
-  console.log(emailInput);
   for (const user in users) {
     if (emailInput === users[user].email){
       return user;
@@ -277,7 +93,7 @@ function doesShortURLExist (shortUrl) {
 }
 
 function fixURL (url) {
-  if (url.substring(0,6) !== 'http://' || url.substring(0,7) !== 'https://') {
+  if (url.substring(0, 6) !== 'http://' || url.substring(0, 7) !== 'https://') {
     return ("http://" + url);
   } else {
     return url;
@@ -294,7 +110,177 @@ function returnUrlListForUser (user) {
   return userUrls;
 }
 
-//initializes the server
+//ROUTING
+
+
+app.get("/", (req, res) => {
+  res.redirect("/urls");
+});
+
+
+//LOGIN
+
+app.get("/login", (req, res) => {
+  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
+  if (!req.session.user_id){
+    res.render("login", templateVars);
+  } else {
+    res.redirect("/urls");
+  }
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const userId = doesEmailExist(email);
+  if (userId){
+    if (bcrypt.compareSync(password, users[userId].password)) {
+      req.session.user_id = users[userId].id;
+      res.redirect('/urls');
+    } else {
+      res.statusCode = 403;
+      res.send('password is incorrect');
+    }
+  } else {
+    res.statusCode = 403;
+    res.send('the username does not exist');
+  }
+});
+
+//LOGOUT
+
+app.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect("/urls");
+});
+
+//REGISTER
+
+app.get("/register", (req, res) => {
+  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
+  if (!req.session.user_id){
+    res.render("register", templateVars);
+  } else {
+    res.redirect("/urls");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const userId = randomString();
+  
+  if (req.body.email && req.body.password) {
+    if (doesEmailExist(req.body.email)) {
+      res.statusCode = 400;
+      res.end("Email already exists as a user. Please login with your email and password.");
+    } else {
+      users[userId] = {
+        id: userId,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10)
+      };
+      req.session.user_id = users[userId].id;
+      res.redirect("/urls");
+    }
+  } else {
+    res.statusCode = 400;
+    res.end("Error: Enter a valid email and password");
+  }
+});
+
+//INDEX
+
+app.get("/urls", (req, res) => {
+  let userUrls = returnUrlListForUser(req.session.user_id);
+  let templateVars = { urls: userUrls, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
+  if (req.session.user_id) {
+    res.render("urls_index", templateVars);
+  } else {
+    res.redirect("/login");
+  }
+});
+
+//NEW URL
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
+  if (req.session.user_id) {
+    res.render("urls_new", templateVars);
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
+  const currentUser = req.session.user_id;
+  const shortUrl = randomString();
+  const longURL = fixURL(req.body.longURL);
+  urlDatabase[shortUrl] = {longUrl: longURL, owner: currentUser };
+  res.redirect("/urls/" + shortUrl);
+});
+
+//URL INFO & UPDATE
+
+app.get("/urls/:id", (req, res) => {
+  let urlId = req.params.id;
+  const currentUser = req.session.user_id;
+  if (doesShortURLExist(urlId)) {
+    if (isAuthorizedtoChange(currentUser, urlId)) {
+      let longURL = urlDatabase[urlId].longUrl;
+      let templateVars = { shortURL: urlId, longURL: longURL, urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
+      res.render("urls_show", templateVars);
+    } else {
+      res.statusCode = 401;
+      res.send('You are not authorized to view this URL. You do not own it or are not logged in.');
+    }
+  } else {
+    res.statusCode = 404;
+    res.send('Short URL does not exist.');
+  }
+});
+
+app.post("/urls/:id/update", (req, res) => {
+  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
+  const shortUrlToUpdate = req.params.id;
+  const updatedLongURL = fixURL(req.body.longURL);
+  const currentUser = req.session.user_id;
+  if ( isAuthorizedtoChange(currentUser, shortUrlToUpdate)) {
+    urlDatabase[shortUrlToUpdate].longUrl = updatedLongURL;
+    res.redirect("/urls");
+  } else {
+    res.statusCode = 401;
+    res.end("You are not authorized to edit this URL. Please login if you own it.");
+  }
+});
+
+
+//DELETE
+
+app.post("/urls/:id/delete", (req, res) => {
+  let templateVars = { urls: urlDatabase, user: req.session.user_id, email: users[req.session.user_id] ? users[req.session.user_id].email : null };
+  const urlToDelete = req.params.id;
+  const currentUser = req.session.user_id;
+  if ( isAuthorizedtoChange(currentUser, urlToDelete)) {
+    delete urlDatabase[urlToDelete];
+    res.redirect("/urls");
+  } else {
+    res.statusCode = 401;
+    res.send("You are not authorized to delete this URL. Please Login.");
+  }
+});
+
+//REDIRECT SHORT URL
+
+app.get("/u/:shortURL", (req, res) => {
+  if (urlDatabase[req.params.shortURL]) {
+    res.redirect(urlDatabase[req.params.shortURL].longUrl);
+  } else {
+    res.statusCode = 404;
+    res.end("Error - Does not exist - Try a different URL or create a new one");
+  }
+});
+
+//INITIALIZE SERVER
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
